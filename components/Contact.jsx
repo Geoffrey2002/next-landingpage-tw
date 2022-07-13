a5898620b79430a244441e39c4f0a91ce99f8b37
Geoffrey Mwangi <gmwangi3174@gmail.com>
2022-07-13 16:58:19 +0300

    added the contact form and page

---
+++ components/Contact.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="max-w-[1240px] m-auto p-4 h-screen">
+      <h1 className="p-4 text-2xl font-bold text-center">
+        Let's work together
+      </h1>
+      <form className="max-w-[600px] m-auto">
+        <div className="grid grid-cols-2 gap-2">
+          <input
+            className="p-3 border shadow-lg"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="p-3 border shadow-lg"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <input
+          className="w-full p-3 my-2 border shadow-lg"
+          type="text"
+          placeholder="Subject"
+        />
+        <textarea
+          className="w-full p-3 border shadow-lg"
+          cols="30"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="w-full p-3 mt-2 border shadow-lg">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Contact from "../components/Contact";
+import Hero from "../components/Hero";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const contact = () => {
+  return (
+    <div>
+      <Hero
+        heading="Contact"
+        message="Submit the form below for more work and quotes."
+      />
+      <Contact />
+    </div>
+  );
 };
 
-export default Contact;
+export default contact;
